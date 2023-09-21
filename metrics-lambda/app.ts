363cc20bab51383981638738c92ad40c3d4db4fc
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-09-21 01:41:25 -0600

    Initial commit. Moved Storage over to DynamoDB

--- metrics-lambda/app.ts
+++ metrics-lambda/app.ts
@@ -1,29 +1,42 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+import GithubMetrics from './types/GithubMetrics';
+import Storage from './utils/Storage';
+import { getGHUserMetrics } from './utils/GithubCommands';
+import Logger from './utils/Logger';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        const GH_API_KEY_1: string = process.env.GH_API_KEY_1 as string;
+        const GH_USERNAME_1: string = process.env.GH_USERNAME_1 as string;
+        const GH_API_KEY_2: string = process.env.GH_API_KEY_2 as string;
+        const GH_USERNAME_2: string = process.env.GH_USERNAME_2 as string;
+
+        Logger.appendDebugLog('Lambda invoked');
+
+        const job1Results: GithubMetrics = await getGHUserMetrics(GH_USERNAME_1, GH_API_KEY_1);
+        const job2Results: GithubMetrics = await getGHUserMetrics(GH_USERNAME_2, GH_API_KEY_2);
+
+        const aggregateMetrics: GithubMetrics = {
+            commits: job1Results.commits + job2Results.commits,
+            mergedPRs: job1Results.mergedPRs + job2Results.mergedPRs,
+            linesOfCodeWritten: job1Results.linesOfCodeWritten + job2Results.linesOfCodeWritten,
+            repositoriesContributed: job1Results.repositoriesContributed + job2Results.repositoriesContributed,
+        };
+
+        Storage.store('metrics', aggregateMetrics);
+
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world',
+                message: aggregateMetrics,
             }),
         };
-    } catch (err) {
-        console.log(err);
+    } catch (err: any) {
+        Logger.appendError(err);
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'some error happened',
+                message: `An error has occured: ${err}`,
             }),
         };
     }
---
+++ metrics-lambda/utils/Storage.ts
@@ -0,0 +1,92 @@
+import Logger from './Logger';
+import * as AWS from 'aws-sdk';
+
+class Storage {
+    private static readonly DATASTORE_FILE: string = 'datastore.json';
+    private static values: any;
+    private static isLoaded: boolean;
+    private static dynamoDB: AWS.DynamoDB.DocumentClient;
+
+    public static async load(): Promise<boolean> {
+        try {
+            Logger.appendDebugLog('Initializing dynamoDB client.');
+            AWS.config.update({
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                region: process.env.AWS_DEFAULT_REGION,
+            });
+
+            this.dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+            Logger.appendDebugLog('Fetching store_id [0] from gh-metrics-store.');
+            this.values = (
+                await this.dynamoDB
+                    .get({
+                        TableName: 'gh-metrics-store',
+                        Key: {
+                            store_id: 0,
+                        },
+                    })
+                    .promise()
+            )?.Item;
+
+            Logger.appendDebugLog(`Fetched values: ${JSON.stringify(this.values)}`);
+
+            if (!this.values || Object.keys(this.values).length === 0) {
+                this.values = { store_id: 0 };
+                Logger.appendDebugLog(
+                    `Nothing to fetch from gh-metrics-store. Creating value [${JSON.stringify(this.values)}].`,
+                );
+                await this.dynamoDB
+                    .put({
+                        TableName: 'gh-metrics-store',
+                        Item: this.values,
+                    })
+                    .promise();
+            }
+
+            this.isLoaded = true;
+            return true;
+        } catch (error: any) {
+            Logger.appendError(`Error loading from storage. Error: ${error}`);
+            return false;
+        }
+    }
+
+    public static async get(key: string): Promise<any> {
+        if (!this.isLoaded) {
+            const success: boolean = await this.load();
+
+            if (!success) {
+                Logger.appendError(`Unable to load to fetch value [${key}] from storage.`);
+            }
+        }
+
+        return this.values[key];
+    }
+
+    public static async store(key: string, value: any): Promise<void> {
+        if (!this.isLoaded) {
+            const success: boolean = await this.load();
+
+            if (!success) {
+                Logger.appendError(`Unable to load to store key/value [${key}]: [${value}] in storage.`);
+            }
+        }
+
+        try {
+            this.values[key] = value;
+            Logger.appendDebugLog('Storing values to gh-metrics-store.');
+            await this.dynamoDB
+                .put({
+                    TableName: 'gh-metrics-store',
+                    Item: this.values,
+                })
+                .promise();
+        } catch (error: any) {
+            Logger.appendError(`Error storing key/value [${key}]: [${value}]. Error: ${error}`);
+        }
+    }
+}
+
+export default Storage;
