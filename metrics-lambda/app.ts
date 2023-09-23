685c775b2a73590bb9469c07b57c178539978b83
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2023-09-23 12:20:33 -0600

    Changed initialization pattern for storage so load only happens on app start

--- metrics-lambda/app.ts
+++ metrics-lambda/app.ts
@@ -6,12 +6,18 @@ import Logger from './utils/Logger';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        Logger.appendDebugLog('Lambda invoked');
+
+        try {
+            await Storage.load();
+        } catch (error) {
+            throw new Error('Could not initialize gh-metrics-store connection.');
+        }
+
         const GH_API_KEY_1: string = process.env.GH_API_KEY_1 as string;
         const GH_USERNAME_1: string = process.env.GH_USERNAME_1 as string;
         const GH_API_KEY_2: string = process.env.GH_API_KEY_2 as string;
         const GH_USERNAME_2: string = process.env.GH_USERNAME_2 as string;
-
-        Logger.appendDebugLog('Lambda invoked');
 
         const job1Results: GithubMetrics = await getGHUserMetrics(GH_USERNAME_1, GH_API_KEY_1);
         const job2Results: GithubMetrics = await getGHUserMetrics(GH_USERNAME_2, GH_API_KEY_2);
@@ -28,7 +34,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         try {
             await Storage.synchronize();
         } catch (error) {
-            Logger.appendError(`Unable to store fetched values to gh-metrics-store. Error: ${error}`);
+            throw new Error(`Unable to synchronize values to gh-metrics-store. Error: ${error}`);
         }
 
         return {
